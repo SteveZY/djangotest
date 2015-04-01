8ea5b62ae9035bc41977297fd1ac13bd56a19b16
SteveZ <ubuntu2000@gmail.com>
2015-04-01 11:22:45 +0800

    added more backup files not related to Django

--- calci.py
+++ calci.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
+BBBBBB# -*- coding: utf-8 -*-
 def chazh():
     i = 0
     while i < 100:
         if i%28 == 0 and i != 0: #i % 3==0 and (100-i)%4 ==0:
             if i  < 100:
                 print 'Then possible cock num should be %d' % (i / 7)
-        i=i+28
-def decfunc(): 
+        ():
+i=i+28
+def decf
 #3x+4y=100 求解
     i = 0 
     while i < 100:
@@ -22,9 +23,20 @@ def times(cash):
         n = n +1
     return int(n + cash/5000)
 
+def yangin20y():
+    n = 1
+    y=[1,0,0,0]
+    while n <= 20:
+        y[3] = y[2]
+        y[2] = y[1]
+        y[1] = y[0]
+        y[0] = y[1] + y[3]
+        n=n+1
+    return sum(y)    
+
 if __name__=="__main__":
 #    chazh()
 #    decfunc()
     print times(100000)
-
+    print 'total sheep num is %d' % yangin20y()
 
--- test.php
+++ test.php
@@ -1,10 +1,27 @@
 <?php
-echo hello.'<br />';
+error_reporting(E_ALL);
+echo 'hello'.'<br />';
 for($i=1;$i <=9; $i++){
 	echo '<br />';
 	for($j=1;$j<=$i;$j++){
 		echo $j,'x',$i,'=',$j*$i,'&nbsp;';
 	}
 }
+
+$a=123;
+$b=$c+1;
+echo '<br />';
+$arr=array('key1'=>'name','key2'=>'sex','gender');
+print_r($arr);
+
+$yang = array(1,0,0,0);
+for($i=1;$i<=20;$i++){
+	array_unshift($yang,0);
+	array_pop($yang);
+	$yang[0] = $yang[1] + $yang[3];
+	echo '<br />';
+	print_r($yang);
+}
+
 ?>
 
